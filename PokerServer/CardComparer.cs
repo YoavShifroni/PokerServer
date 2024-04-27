815811f3691c1259d8661202a9f0ad3790eb4d1c
YoavShifroni <123202892+YoavShifroni@users.noreply.github.com>
2024-04-27 17:01:29 +0300

    Update CardComparer.cs

--- PokerServer/CardComparer.cs
+++ PokerServer/CardComparer.cs
@@ -9,7 +9,7 @@ namespace PokerServer
     public class CardComparer : IComparer<Card>
     {
         /// <summary>
-        /// Imlementation of the Compare function to compare between 2 cards, based on their values
+        /// Imlpementation of the Compare function to compare between 2 cards, based on their values
         /// </summary>
         /// <param name="x">Card to compare</param>
         /// <param name="y">Card to compare</param>
